3fcbb73f786bce4ad53109b69e22353bce9ca65f
utsubyo <72735425+ustubyo@users.noreply.github.com>
2021-02-01 21:11:10 +0800

    Initial commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    float movespeed = 5f;
+
+    public Rigidbody Playerrb;
+    public Animator Playeranim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+    }
+    void movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * movespeed);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            Playeranim.SetBool("isrun", true);
+            
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * movespeed);
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            Playeranim.SetBool("isrun", true);
+
+        }
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
+        {
+            Playeranim.SetBool("isrun", false);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * movespeed);
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+            Playeranim.SetBool("isrun", true);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * movespeed);
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+            Playeranim.SetBool("isrun", true);
+        }
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            Playeranim.SetBool("isrun", false);
+        }
+    }
+}
