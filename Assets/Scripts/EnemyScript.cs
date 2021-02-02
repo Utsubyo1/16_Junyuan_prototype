79d8a00d740273eb40460a7f32b1457dce69527f
utsubyo <72735425+ustubyo@users.noreply.github.com>
2021-02-02 21:11:30 +0800

    update enemy ai

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public Transform player;
+    float speed = 1f;
+    Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Enmey move towards the player
+        Vector3 pos = Vector3.MoveTowards(transform.position, player.position, speed * Time.fixedDeltaTime);
+        rb.MovePosition(pos);
+        transform.LookAt(player);
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     float movespeed = 5f;
+    float rotatespeed = 2f;
 
+    int maxspeed = 5;
     public Rigidbody Playerrb;
     public Animator Playeranim;
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,20 +22,23 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
         movement();
+        //submovement();
     }
     void movement()
     {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.forward * Time.deltaTime * movespeed);
-            transform.rotation = Quaternion.Euler(0, 0, 0);
+            //transform.rotation = Quaternion.Euler(0, 0, 0);
             Playeranim.SetBool("isrun", true);
             
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.forward * Time.deltaTime * movespeed);
-            transform.rotation = Quaternion.Euler(0, 180, 0);
+            transform.Translate(Vector3.back * Time.deltaTime * movespeed);
+            //transform.rotation = Quaternion.Euler(0, 180, 0);
             Playeranim.SetBool("isrun", true);
 
         }
@@ -42,19 +49,40 @@ public class PlayerScript : MonoBehaviour
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.forward * Time.deltaTime * movespeed);
-            transform.rotation = Quaternion.Euler(0, -90, 0);
+            //transform.Translate(Vector3.forward * Time.deltaTime * movespeed);
+            //transform.rotation = Quaternion.Euler(0, -90, 0);
+            transform.Rotate(new Vector3(0, h * rotatespeed, 0));
             Playeranim.SetBool("isrun", true);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.forward * Time.deltaTime * movespeed);
-            transform.rotation = Quaternion.Euler(0, 90, 0);
+            //transform.Translate(Vector3.forward * Time.deltaTime * movespeed);
+            //transform.rotation = Quaternion.Euler(0, 90, 0);
+            transform.Rotate(new Vector3(0, h * rotatespeed, 0));
             Playeranim.SetBool("isrun", true);
         }
         if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
         {
             Playeranim.SetBool("isrun", false);
+        }
+    }
+
+    void submovement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Rotate(new Vector3(0, h * rotatespeed, 0));
+
+        Playerrb.AddForce(v * transform.forward * movespeed);
+
+        Playerrb.velocity = new Vector3(Mathf.Clamp(Playerrb.velocity.x, -maxspeed, maxspeed),
+            Mathf.Clamp(Playerrb.velocity.y, -maxspeed, maxspeed),
+            Mathf.Clamp(Playerrb.velocity.z, -maxspeed, maxspeed));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
         }
     }
 }
