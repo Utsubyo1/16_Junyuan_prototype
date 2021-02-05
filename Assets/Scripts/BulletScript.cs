211bad760526b2eee7f1fe52e4bcacb6a7234c41
utsubyo <72735425+ustubyo@users.noreply.github.com>
2021-02-05 21:50:50 +0800

    Reloading added

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 public class BulletScript : MonoBehaviour
 {
     float speed = 2f;
-    
-    
+   
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 2);
-
+        Destroy(gameObject, 1);
     }
 
     // Update is called once per frame
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,23 +5,39 @@ using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
+    //movement and rotate speed
     float movespeed = 5f;
     float rotatespeed = 2f;
+
+    //Ammo count
+    public int maxAmmo = 5;
+    private int currentAmmo;
+    public float reloadtime = 10f;
+    private bool isreloading = false;
 
     public int maxHealth = 100;
     public int currentHealth;
 
+    
+    public AudioClip[] AudioClipsArr;
+    private AudioSource audiosource;
     public Text Hptext;
     public HealthbarScript healthBar;
     public Rigidbody Playerrb;
     public Animator Playeranim;
 
+    //bullet spawn
     public GameObject bulletPrefab;
     public GameObject bulletSpawn;
 
     // Start is called before the first frame update
     void Start()
     {
+        //check adn set ammo when start
+        currentAmmo = maxAmmo;
+
+        audiosource = GetComponent<AudioSource>();
+        //check, set and display health when start
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
         Hptext.GetComponent<Text>().text = (maxHealth + "/" + currentHealth);
@@ -32,7 +48,17 @@ public class PlayerScript : MonoBehaviour
     {
         
         movement();
-        
+
+        if (isreloading)
+            return;
+
+        if(currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        //Player death
         if(currentHealth == 0)
         {
             Playeranim.SetTrigger("Death");
@@ -79,14 +105,26 @@ public class PlayerScript : MonoBehaviour
         {
             Playeranim.SetBool("isrun", false);
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isreloading == false)
         {
             Instantiate(bulletPrefab, bulletSpawn.transform.position, transform.rotation);
-            Playeranim.SetTrigger("Fire");  
-           // Hptext.GetComponent<Text>().text = (maxHealth + "/" + currentHealth);
+            Playeranim.SetTrigger("Fire");
+            currentAmmo--;
+            audiosource.PlayOneShot(AudioClipsArr[0]);
+            audiosource.volume = 0.5f;
         }
     }
 
+    IEnumerator Reload()
+    {
+        isreloading = true;
+        Debug.Log("Reloading . . .");
+
+        yield return new WaitForSeconds(reloadtime);
+        audiosource.PlayOneShot(AudioClipsArr[1]);
+        currentAmmo = maxAmmo;
+        isreloading = false;
+    }
    void takeDamage(int damage)
     {
         //Do damage 
